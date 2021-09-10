c19eecef3e66cb82e677fcd9e31be5cf931a0f87
nvite0109 <nvite0109@gmail.com>
2021-09-10 00:43:16 -0500

    proyecto inicial nvite-market

---
+++ src/test/java/com/nvite/market/NviteMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvite.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NviteMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
